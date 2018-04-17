52bcfe6de2f5468a8b35805785cc0f6042902473
pablo <pablo.makoki@gmail.com>
2018-04-18 01:42:52 +0200

    carrete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { ModalController } from 'ionic-angular';
 import { SubirPage } from '../subir/subir';
 
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs/Observable';
+//import { AngularFireDatabase } from 'angularfire2/database';
+//import { Observable } from 'rxjs/Observable';
+
+import { CargaArchivoProvider } from '../../providers/carga-archivo/carga-archivo';
 
 
 @Component({
@@ -11,15 +13,26 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  posts: Observable<any[]>;
+  hayMas: boolean = true;
+  //posts: Observable<any[]>;
 
-  constructor(private modalCtrl: ModalController,  private afDB: AngularFireDatabase) {
-    this.posts = afDB.list('post').valueChanges();
+  constructor(private modalCtrl: ModalController, private _cap: CargaArchivoProvider) {
+    //this.posts = afDB.list('post').valueChanges();
   }
 
   mostrar_modal() {
     let modal = this.modalCtrl.create ( SubirPage );
     modal.present();
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+    this._cap.carga_imagenes().then ( 
+      (hayMas:boolean)  => {
+        console.log(hayMas);
+        this.hayMas = hayMas;
+        infiniteScroll.complete();
+      });    
   }
 
 }
--- src/providers/carga-archivo/carga-archivo.ts
+++ src/providers/carga-archivo/carga-archivo.ts
@@ -5,13 +5,57 @@ import { ToastController } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase';
 
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class CargaArchivoProvider {
 
   imagenes: ArchivoSubir[] = [];
+  lastKey: string;  
 
   constructor(public toastCtrl: ToastController, public afdb: AngularFireDatabase) {
     console.log('Hello CargaArchivoProvider Provider');
+
+    this.cargar_ultimo_key().subscribe( () => this.carga_imagenes());
+  }
+
+  private cargar_ultimo_key() {
+    return this.afdb.list( '/post', ref => ref.orderByKey().limitToLast(1) )
+             .valueChanges()
+             .map( (post:any) => {
+               console.log(post);
+               this.lastKey = post[0].key;
+
+               this.imagenes.push (post[0]);
+             })
+  }
+
+  carga_imagenes() {
+    return new Promise( ( resolve, reject  ) => {
+      this.afdb.list('/post', 
+        ref => ref.limitToLast(3)
+                  .orderByKey()
+                  .endAt( this.lastKey )
+      ).valueChanges()
+       .subscribe( (posts:any) => {
+         posts.pop();
+
+          if (posts.length === 0 ) {
+            console.log('No hay más registros');
+            resolve(false);
+            return;
+          }
+
+          this.lastKey = posts[0].key;
+
+          for (let i = posts.length-1; i>=0; i--) {
+            let post = posts[i];
+            this.imagenes.push (post);
+          }
+
+          resolve(true);
+       });
+    });
   }
 
   cargar_imagen_firebase(archivo: ArchivoSubir) {
